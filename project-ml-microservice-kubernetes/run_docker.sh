e762f94ad774666e29769d6b953805a1bd368eb1
maurice1er <armeldrey@gmail.com>
2022-08-03 07:34:13 +0000

    update docker script

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t ml_app:1.0 .
 
 # Step 2: 
 # List docker images
+docker images 
 
 # Step 3: 
 # Run flask app
+docker run -itd -p 8000:80 --name ml_app ml_app:1.0 
+
